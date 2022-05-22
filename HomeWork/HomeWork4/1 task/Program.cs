ac61399dbeb8478126db7efd0ffc86042bf02e90
Oleksii <zankexd@icloud.com>
2022-05-22 16:49:03 +0200

    home2

--- HomeWork/HomeWork4/1 task/Program.cs
+++ HomeWork/HomeWork4/1 task/Program.cs
@@ -1,8 +1,25 @@
-﻿double SumAll (double A, double b)
+﻿//double SumAll (double A, double b)
+//{
+//    double result = Math.Pow(A,b);
+//    return result;
+//}
+//Console.WriteLine (SumAll(5,7));
+//Console.WriteLine (SumAll(3,15));
+//Console.WriteLine (SumAll(2,7));
+int SumAll (int A, int b)
 {
-    double result = Math.Pow(A,b);
-    return result;
+    int result =A;
+    
+    for (int i = 1; i <b;i++)
+    {
+        
+         result *=A;
+
+     
+    }
+    
+  return result;
+    
 }
-Console.WriteLine (SumAll(5,7));
-Console.WriteLine (SumAll(3,15));
-Console.WriteLine (SumAll(2,7));
+
+Console.Write(SumAll(3,5));
