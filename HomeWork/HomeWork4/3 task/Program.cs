6f2e22da49bc37a66dd441bb3bc561c8b46de66a
Oleksii <zankexd@icloud.com>
2022-05-22 18:26:04 +0200

    hom53

--- HomeWork/HomeWork4/3 task/Program.cs
+++ HomeWork/HomeWork4/3 task/Program.cs
@@ -5,8 +5,29 @@
     {
          array[i]=new Random().Next(1,10);
         Console.Write( "."+array[i]);
-    }
-    
-    
+    }   
 }
 RanndomMasiv();
+
+
+void Vyvod(int[] array)
+{
+     
+    for (int i = 0;i<array.Length;i++)
+    {
+        Console.Write(array[i]+",");
+    }
+}
+
+int[]Masiv (int Dlina,int min, int max)
+{
+    int[] array=new int [Dlina];
+    for(int i =0;i<Dlina;i++)
+    {
+        array[i]=new Random().Next(min,max);
+    }
+    return array;
+    
+}
+
+Vyvod(Masiv(8,1,10));
