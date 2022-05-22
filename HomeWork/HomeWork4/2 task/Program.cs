c187af2c52e4fd2cf08a78cdfe87541feae706e8
Oleksii <zankexd@icloud.com>
2022-05-22 17:46:19 +0200

    home3

--- HomeWork/HomeWork4/2 task/Program.cs
+++ HomeWork/HomeWork4/2 task/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿int Summ (int A)
+{
+    
+    int result=0;
+    
+   while (A>0)
+    {
+        result+=A%10;
+        A/=10;
+
+        
+        
+    }
+  
+  return result;
+}
+
+Console.Write(Summ(12336));
