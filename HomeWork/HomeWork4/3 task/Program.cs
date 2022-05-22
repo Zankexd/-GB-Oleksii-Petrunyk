2671444961b1b1d40de467f1b5adb5689967d11c
Oleksii <zankexd@icloud.com>
2022-05-22 18:01:40 +0200

    hom43

---
+++ HomeWork/HomeWork4/3 task/Program.cs
@@ -0,0 +1,12 @@
+﻿void RanndomMasiv()
+{
+    int[]array = new int [8];
+    for (int i = 0;i<array.Length;i++)
+    {
+         array[i]=new Random().Next(1,10);
+        Console.Write( "."+array[i]);
+    }
+    
+    
+}
+RanndomMasiv();
