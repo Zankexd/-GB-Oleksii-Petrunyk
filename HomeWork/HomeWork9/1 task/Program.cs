dae55ee36a85642a011728c80e4246abcb5fea55
Oleksii <zankexd@icloud.com>
2022-06-11 23:06:56 +0200

    Finish2

---
+++ HomeWork/HomeWork9/1 task/Program.cs
@@ -0,0 +1,11 @@
+﻿void ElementLenght(int m,int n)
+{
+    if(n<m)return;
+    if(n%2<1)
+    {
+        Console.Write($"{n},");
+    }
+
+    ElementLenght(m,n-1);
+}
+ElementLenght(2,10);
---
+++ HomeWork/HomeWork9/2 task/Program.cs
@@ -0,0 +1,16 @@
+﻿int ElementLenght(int m,int n)
+{    
+    
+   int sum=0;
+   
+    if(n<m)return 0;
+    
+    
+    return ElementLenght(m,n-1)+n;
+
+    
+  
+
+    
+}
+Console.Write(ElementLenght(4,8));
